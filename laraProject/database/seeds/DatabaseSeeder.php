f53c1591f7f45e647a4212229d138057ae5c55ae
Matteo Straccia <Matteo Straccia@LAPTOP-UDJNAOF5.home>
2022-05-15 15:24:18 +0200

    Modifica date Seeder

--- laraProject/database/seeds/DatabaseSeeder.php
+++ laraProject/database/seeds/DatabaseSeeder.php
@@ -20,18 +20,18 @@ class DatabaseSeeder extends Seeder
        
        DB::table('accomodations')->insert([
            ['id'=>1,'nome'=>"Appartamento carino sul Lago di Garda",'descr'=> "Appartamento accogliente e gradevole. Situato nei pressi del lago, raggiungibile in 3 minuti di macchina. Presenta un giardino ampio e gli interni sono ben arredati con forniture dallo stile del 19 secolo ",
-               'tipologia'=>'App','citta'=>"Brescia",'prov'=>"BS",'via'=>"Via T.Tasso",'num_civ'=>80,'sup'=>100,'inizio_disp'=>16-05-2022,'fine_disp'=>19-07-2022,'data_ins'=>15-05-2022,'eta_min'=>18,'eta_max'=>70,'sesso'=>"M",'canone'=>156.00,'posti_letto'=>6,'cucina'=>'true','num_bagni'=>3,'num_camere'=>4,'locale_ricreativo'=>'true','angolo_studio'=>'true',
+               'tipologia'=>'App','citta'=>"Brescia",'prov'=>"BS",'via'=>"Via T.Tasso",'num_civ'=>80,'sup'=>100,'inizio_disp'=>2022-05-16,'fine_disp'=>2022-07-19,'data_ins'=>2022-05-15,'eta_min'=>18,'eta_max'=>70,'sesso'=>"M",'canone'=>156.00,'posti_letto'=>6,'cucina'=>'true','num_bagni'=>3,'num_camere'=>4,'locale_ricreativo'=>'true','angolo_studio'=>'true',
                'proprietario' => "Giancarlo Magalli"],
            ['id'=>2,'nome'=>"Posto letto su casa di mare",'descr'=> "Camera per studenti. Situata nei pressi di Ancona est, vicinissima al mare. Vicino a punti di snodo per raggiungere le universtià di tutta la città.",
-               'tipologia'=>'Pls','citta'=>"Ancona",'prov'=>"AN",'via'=>"Via San Tommaso",'num_civ'=>2,'sup'=>23,'inizio_disp'=>12-10-2022,'fine_disp'=>19-10-2022,'data_ins'=>15-05-2022,'eta_min'=>18,'eta_max'=>27,'sesso'=>"F",'canone'=>15.00,'posti_letto'=>1,'cucina'=>'false','num_bagni'=>1,'num_camere'=>1,'locale_ricreativo'=>'false','angolo_studio'=>'true',
+               'tipologia'=>'Pls','citta'=>"Ancona",'prov'=>"AN",'via'=>"Via San Tommaso",'num_civ'=>2,'sup'=>23,'inizio_disp'=>2022-10-12,'fine_disp'=>2022-10-19,'data_ins'=>2022-05-15,'eta_min'=>18,'eta_max'=>27,'sesso'=>"F",'canone'=>15.00,'posti_letto'=>1,'cucina'=>'false','num_bagni'=>1,'num_camere'=>1,'locale_ricreativo'=>'false','angolo_studio'=>'true',
                'proprietario' => "Cristian Notts"],
        ]);
                
        DB::table('users')->insert([
-           ['id'=>1,'nome'=>"Matteo",'cognome'=>"Straccia",'data_nasc'=>24-12-1998,'tipologia'=>"Admin",'username'=>"mattew98",'password'=>"ciao1234"],
-           ['id'=>2,'nome'=>"Reed",'cognome'=>"Richards",'data_nasc'=>12-04-1978,'tipologia'=>"Host",'username'=>"mrfanta04",'password'=>"suestorm"],
-           ['id'=>3,'nome'=>"Norman",'cognome'=>"Osborn",'data_nasc'=>03-08-1981,'tipologia'=>"Loc",'username'=>"greengob1",'password'=>"hatesm"],
-           ['id'=>4,'nome'=>"Layla",'cognome'=>"El-Fouly",'data_nasc'=>14-02-1991,'tipologia'=>"Loc",'username'=>"sacarlscarab",'password'=>"dad"]
+           ['id'=>1,'nome'=>"Matteo",'cognome'=>"Straccia",'data_nasc'=>1998-12-24,'tipologia'=>"Admin",'username'=>"mattew98",'password'=>"ciao1234"],
+           ['id'=>2,'nome'=>"Reed",'cognome'=>"Richards",'data_nasc'=>1978-04-12,'tipologia'=>"Host",'username'=>"mrfanta04",'password'=>"suestorm"],
+           ['id'=>3,'nome'=>"Norman",'cognome'=>"Osborn",'data_nasc'=>1981-08-03,'tipologia'=>"Loc",'username'=>"greengob1",'password'=>"hatesm"],
+           ['id'=>4,'nome'=>"Layla",'cognome'=>"El-Fouly",'data_nasc'=>1991-02-14,'tipologia'=>"Loc",'username'=>"sacarlscarab",'password'=>"dad"]
            
        ]);
                
@@ -47,8 +47,8 @@ class DatabaseSeeder extends Seeder
        ]);
        
        DB::table('options')->insert([
-           ['id'=>1,'nota'=>"",'data_invio'=>17-05-2022,'data_inizio'=>13-10-2022,'data_fine'=>19-10-2022,'data_stipula'=>"",'id_alloggio'=>2,'id_locatario'=>3],
-           ['id'=>2,'nota'=>"",'data_invio'=>17-05-2022,'data_inizio'=>13-10-2022,'data_fine'=>19-10-2022,'data_stipula'=>"",'id_alloggio'=>2,'id_locatario'=>4]
+           ['id'=>1,'nota'=>"",'data_invio'=>2022-05-17,'data_inizio'=>2022-10-13,'data_fine'=>2022-10-19,'data_stipula'=>"",'id_alloggio'=>2,'id_locatario'=>3],
+           ['id'=>2,'nota'=>"",'data_invio'=>2022-05-17,'data_inizio'=>2022-10-13,'data_fine'=>2022-10-19,'data_stipula'=>"",'id_alloggio'=>2,'id_locatario'=>4]
        ]);
     }
 }
